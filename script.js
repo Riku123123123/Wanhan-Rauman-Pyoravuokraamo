fdca3b6b888b4c5a39e238b99f8fbed80b293311
Riku123123123 <145870211+Riku123123123@users.noreply.github.com>
2024-06-15 01:07:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -31,35 +31,26 @@ document.addEventListener('DOMContentLoaded', function() {
     // Hamburger-menu-toiminto
     function toggleMenu() {
         const navItems = document.querySelector('.nav-items');
+        const notificationBox = document.getElementById('notification-box');
+        const hamtausta = document.getElementById('hamtausta'); // Lisätään hamtausta-elementti
+
         navItems.classList.toggle('active');
-        document.querySelectorAll('.nav-items a').forEach(item => {
-            item.addEventListener('click', toggleMenu);
-        });
+        notificationBox.classList.remove('active'); // Varmista, että ilmoitus ei ole aktiivinen avattaessa valikko
+
+        if (navItems.classList.contains('active')) {
+            hamtausta.style.display = 'block';
+        } else {
+            hamtausta.style.display = 'none';
+        }
     }
 
-    // Kiinnitetään funktiot globaaleihin muuttujiiin, jotta ne ovat saatavilla HTML:stä
+    // Avaa aloitusosio
+    navigateTo('home');
+
+    // Kiinnitetään funktiot globaaleihin muuttujiin, jotta ne ovat saatavilla HTML:stä
     window.setLanguage = setLanguage;
     window.navigateTo = navigateTo;
     window.toggleMenu = toggleMenu;
-});
-
-document.addEventListener('DOMContentLoaded', function() {
-    // Koodi, joka ajetaan vasta, kun DOM on täysin ladattu
-    // Voit asettaa täällä tapahtumankäsittelijät tai suorittaa muita toimintoja
-
-    function navigateTo(sectionId) {
-        // Piilota kaikki osiot
-        const sections = document.querySelectorAll('section');
-        sections.forEach(section => {
-            section.style.display = 'none';
-        });
-
-        // Näytä valittu osio
-        const targetSection = document.getElementById(sectionId);
-        if (targetSection) {
-            targetSection.style.display = 'block';
-        }
-    }
 
     // Alustetaan navigaatio tapahtumat
     const navItems = document.querySelectorAll('.nav-items a');
@@ -67,104 +58,107 @@ document.addEventListener('DOMContentLoaded', function() {
         item.addEventListener('click', function() {
             const sectionId = this.getAttribute('onclick').replace("navigateTo('", "").replace("')", "");
             navigateTo(sectionId);
+            toggleMenu(); // Sulje valikko kun navigoidaan
         });
     });
-
-    // Avaa aloitusosio
-    navigateTo('home');
 });
 
-document.addEventListener('DOMContentLoaded', () => {
+    // Button animaatio
     const buttons = document.querySelectorAll('button');
-    
     buttons.forEach(button => {
         button.addEventListener('click', () => {
             button.classList.add('animate');
-            
-            // Poistetaan animaatioluokka, jotta se voidaan lisätä uudelleen seuraavalla painalluksella
             setTimeout(() => {
                 button.classList.remove('animate');
             }, 500);
         });
     });
-});
 
-const bikeUrls = {
-    'terässiipi': 'https://twice.shop/wanhanraumanpyrvuokraus/product/voCeBmWjfU8iu3wyRonv',
-    'nopsa': 'https://twice.shop/wanhanraumanpyrvuokraus/product/JCiNiKKAoZf2uZcjABfI',
-    'helkama': 'https://twice.shop/wanhanraumanpyrvuokraus/product/Olp0IztcX7TJTXPGg77k',
-    'madison': 'https://twice.shop/wanhanraumanpyrvuokraus/product/MVCFG2UdZqkmkaASCE6k',
-    'carraro': 'https://twice.shop/wanhanraumanpyrvuokraus/product/Pd3Zvd2TmurVqUUNH3dH',
-    'nakamura': 'https://twice.shop/wanhanraumanpyrvuokraus/product/w1XL8k7ThBrftyOJePLc',
-    'ibrido': 'https://twice.shop/wanhanraumanpyrvuokraus/product/BkXWgHwo7cneQ5DzmEQn'
-};
+    // Modal-toiminnallisuus
+    const bikeUrls = {
+        'terässiipi': 'https://twice.shop/wanhanraumanpyrvuokraus/product/voCeBmWjfU8iu3wyRonv',
+        'nopsa': 'https://twice.shop/wanhanraumanpyrvuokraus/product/JCiNiKKAoZf2uZcjABfI',
+        'helkama': 'https://twice.shop/wanhanraumanpyrvuokraus/product/Olp0IztcX7TJTXPGg77k',
+        'madison': 'https://twice.shop/wanhanraumanpyrvuokraus/product/MVCFG2UdZqkmkaASCE6k',
+        'carraro': 'https://twice.shop/wanhanraumanpyrvuokraus/product/Pd3Zvd2TmurVqUUNH3dH',
+        'nakamura': 'https://twice.shop/wanhanraumanpyrvuokraus/product/w1XL8k7ThBrftyOJePLc',
+        'ibrido': 'https://twice.shop/wanhanraumanpyrvuokraus/product/BkXWgHwo7cneQ5DzmEQn'
+    };
 
-const bikeVideoUrls = {
-    'terässiipi': 'none.mp4',
-    'nopsa': 'none.mp4',
-    'helkama': 'none.mp4',
-    'madison': 'none.mp4',
-    'carraro': 'none.mp4',
-    'nakamura': 'none.mp4',
-    'ibrido': 'none.mp4'
-};
+    const bikeVideoUrls = {
+        'terässiipi': 'none.mp4',
+        'nopsa': 'none.mp4',
+        'helkama': 'none.mp4',
+        'madison': 'none.mp4',
+        'carraro': 'none.mp4',
+        'nakamura': 'none.mp4',
+        'ibrido': 'none.mp4'
+    };
 
-function openModal(name, image, description) {
-    document.getElementById('bikeName').innerText = name;
-    document.getElementById('bikeImage').src = image;
-    document.getElementById('bikeDescription').innerText = description;
+    function openModal(name, image, description) {
+        document.getElementById('bikeName').innerText = name;
+        document.getElementById('bikeImage').src = image;
+        document.getElementById('bikeDescription').innerText = description;
 
-    const url = bikeUrls[name.toLowerCase()] || '';
-    const videoUrl = bikeVideoUrls[name.toLowerCase()] || '';
+        const url = bikeUrls[name.toLowerCase()] || '';
+        const videoUrl = bikeVideoUrls[name.toLowerCase()] || '';
 
-    const bikeURL = document.getElementById('bikeURL');
-    bikeURL.href = url;
+        const bikeURL = document.getElementById('bikeURL');
+        bikeURL.href = url;
 
-    const bikeVideoSource = document.getElementById('bikeVideoSource');
-    bikeVideoSource.src = videoUrl;
+        const bikeVideoSource = document.getElementById('bikeVideoSource');
+        bikeVideoSource.src = videoUrl;
 
-    const bikeVideo = document.getElementById('bikeVideo');
-    bikeVideo.load(); // Load the new video URL
-    bikeVideo.pause(); // Play the video
+        const bikeVideo = document.getElementById('bikeVideo');
+        bikeVideo.load(); // Load the new video URL
+        bikeVideo.pause(); // Pause the video
 
-    const modal = document.getElementById('bikeModal');
-    modal.style.display = "block";
-    modal.classList.remove('modal-close');
-    modal.classList.add('modal-open');
-}
-
-
-function closeModal() {
-    const modal = document.getElementById('bikeModal');
-    const bikeVideo = document.getElementById('bikeVideo');
-    bikeVideo.pause(); // Pause the video when closing the modal
-
-    modal.classList.remove('modal-open');
-    modal.classList.add('modal-close');
-    setTimeout(() => {
-        modal.style.display = "none";
-    }, 500); // Sama kesto kuin modalClose-animaatiolla
-}
-  
-// Close the modal when the user clicks outside of it
-window.onclick = function(event) {
-    const modal = document.getElementById('bikeModal');
-    if (event.target == modal) {
-        closeModal();
+        const modal = document.getElementById('bikeModal');
+        modal.style.display = "block";
+        modal.classList.remove('modal-close');
+        modal.classList.add('modal-open');
     }
-}
 
+    function closeModal() {
+        const modal = document.getElementById('bikeModal');
+        const bikeVideo = document.getElementById('bikeVideo');
+        bikeVideo.pause(); // Pause the video when closing the modal
 
-document.addEventListener('DOMContentLoaded', (event) => {
+        modal.classList.remove('modal-open');
+        modal.classList.add('modal-close');
+        setTimeout(() => {
+            modal.style.display = "none";
+        }, 500); // Sama kesto kuin modalClose-animaatiolla
+    }
+
+    // Sulje modal klikkaamalla ulkopuolella
+    window.onclick = function(event) {
+        const modal = document.getElementById('bikeModal');
+        if (event.target == modal) {
+            closeModal();
+        }
+    };
+
+    // Ilmoitusnäyttö
+    function showNotification() {
+        const notificationBox = document.getElementById('notification-box');
+        notificationBox.style.display = 'none'; // Muuta 'none' -> 'block' halutessasi näyttää ilmoituslaatikon
+    }
+
+    function closeNotification() {
+        const notificationBox = document.getElementById('notification-box');
+        notificationBox.style.display = 'none';
+    }
+
+    // Käynnistä ilmoitus
     showNotification();
-});
 
-function showNotification() {
-    const notificationBox = document.getElementById('notification-box');
-    notificationBox.style.display = 'none';  // TÄHÄN KOHTAAN 'block' NIIN SAA ILMOITUSLAATIKON PÄÄLLE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-}
+    // Lisätään kuuntelija, joka sulkee valikon kun klikataan muualle sivulle
+    document.addEventListener('click', function(event) {
+        var navItems = document.querySelector('.nav-items');
+        if (!event.target.closest('.hamburger') && !event.target.closest('.nav-items')) {
+            navItems.classList.remove('active');
+            hamtausta.style.display = 'none';
+        }
+    });
 
-function closeNotification() {
-    const notificationBox = document.getElementById('notification-box');
-    notificationBox.style.display = 'none';
-}
